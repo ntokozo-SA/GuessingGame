c41f66c2476b5a60ed514eb58a11f4d5ef052ccd
ntokozo-SA <Ntokozobuthelezi205@gmail.com>
2025-04-30 15:09:17 +0200

    final commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const minNum = 1;
+const maxNum = 100;
+const answer = Math.floor(Math.random() * (maxNum - minNum) + 1);
+
+console.log(answer);
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while (running) {
+
+    guess = window.prompt(`Guess a number between ${minNum} and ${maxNum}`);
+
+    if(isNaN(guess)) {
+        window.alert("Please enter a number.");
+    }
+    else if (guess < minNum || guess > maxNum){
+        window.alert(`Please enter a number between ${minNum} and ${maxNum}.`);
+    }
+    else {
+        attempts++;
+        if(guess < answer){
+            window.alert("Too low! Try again.");    
+        }
+        else if(guess > answer){
+            window.alert("Too high! Try again.");
+        }
+        else {
+            window.alert(`Congratulations! You guessed the number in ${attempts} attempts.`);
+            running = false;
+        }
+    }
+    
+   
+}
